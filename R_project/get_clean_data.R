7c1e8d8f60250caeea7faf34d457a0153f5d28f8
Jacob Callahan <jacobwcallahan@gmail.com>
2025-04-23 21:43:15 -0500

    Did some model dev. added get_clean_data.R, did some other things

---
+++ R_project/get_clean_data.R
@@ -0,0 +1,81 @@
+library(dplyr)
+library(stringr)
+
+get_clean_data = function() {
+  # Accessing Dataset
+  csv_data = as.data.frame(read.csv(here::here("final_dataset.csv")))
+  unclean_data = csv_data
+  
+  unclean_data$DamagedProperty = as.factor(get_prop_damage_cats(unclean_data$DamagedProperty1))
+  
+  # Replaces (N/A) with Unable to Determine in Cause1 vector
+  unclean_data$Cause1 = ifelse(unclean_data$Cause1 == "(N/A)",
+                               "Unable to Determine", unclean_data$Cause1)
+  
+  # Drops CrashSeverityCD and CrashInjurySeverity as they have collinearity with CrashSeverity
+  unclean_data = unclean_data[,-c(1,2)]
+  
+  # Swapping CrashSeverity to first column
+  unclean_data = unclean_data[, c("CrashSeverity", "TotalInjured", 
+                                  setdiff(names(unclean_data), 
+                                          c("TotalInjured", "CrashSeverity")))]
+  
+  # This drops TimeOfCrash and DamagedProperty1. These variables need to be fixed to conduct analysis on. 
+  unclean_data = unclean_data[,!colnames(unclean_data) %in% c("DamagedProperty1","CityClassCode","CityName","CityClassCode","CrashReportCounty","TotalInjured","TrafficControlDeviceCond","ClassOfTrafficway")] 
+  
+  unclean_data$TimeOfCrash = as.numeric(sapply(strsplit(time,":"), function(x) x[1]))
+  
+  colnames(unclean_data)[colnames(unclean_data) == "TimeOfCrash"] = "HourOfCrash"
+  
+  orig_data = unclean_data
+  
+  cols = colnames(orig_data)
+  
+  num_vars = c("CrashSeverityCd","CityClassCode","TotalInjured")
+  
+  for (i in 1:length(orig_data)) {
+    if (!colnames(orig_data)[i] %in% num_vars) {
+      orig_data[,i] = as.factor(orig_data[,i,drop = TRUE])
+    }
+  }
+  
+  return(orig_data)
+  
+}
+
+get_prop_damage_cats = function(damage_col) {
+  # Step 1: Lowercase and clean
+  damage_col = tolower(damage_col)
+  damage_col[damage_col == " "] <- "none"
+  
+  # Step 2: Categorize using keyword mapping
+  damage_cat <- case_when(
+    str_detect(damage_col, "deer") ~ "deer",
+    str_detect(damage_col, "sign") ~ "sign",
+    str_detect(damage_col, "pole|poll|light") ~ "pole",
+    str_detect(damage_col, "tree") ~ "tree",
+    str_detect(damage_col, "fence|gate|fencing") ~ "fence",
+    str_detect(damage_col, "median|curb|divider") ~ "median",
+    str_detect(damage_col, "guard ?rail|gaurd ?rail") ~ "guardrail",
+    str_detect(damage_col, "unknown") ~ "none",
+    str_detect(damage_col, "post") ~ "post",
+    str_detect(damage_col, "trailer") ~ "vehicle",
+    str_detect(damage_col, "hydrant") ~ "hydrant",
+    str_detect(damage_col, "grass|lawn|field|yard|landscaping") ~ "lawn",
+    str_detect(damage_col, "mailbox") ~ "mailbox",
+    str_detect(damage_col, "wall") ~ "wall",
+    str_detect(damage_col, "cable") ~ "cable",
+    str_detect(damage_col, "garage|house|building|residence") ~ "building",
+    str_detect(damage_col, "barrier") ~ "barrier",
+    str_detect(damage_col, "bridge") ~ "bridge",
+    str_detect(damage_col, "bicycle|bike") ~ "bike",
+    str_detect(damage_col, "barrels") ~ "barrels",
+    str_detect(damage_col, "ditch") ~ "ditch",
+    str_detect(damage_col, "object") ~ "object",
+    str_detect(damage_col, "rail") ~ "rail",
+    str_detect(damage_col, "none") ~ "none",
+    TRUE ~ "other"
+  )
+  
+  return(damage_cat)
+}
